3d4a1929ced74a3ba0daee83ef0bc2dd59e6b287
Anthony Rende <rende718@gmail.com>
2023-05-08 11:01:32 -0400

    Generate random number return html response

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    extract::Query,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use rand::{thread_rng, Rng};
+use serde::Deserialize;
 use std::net::SocketAddr;
 // use std::net::SocketAddr;
 // use tower_http::cors::{AllowedOrigins, CorsLayer};
 
 #[tokio::main]
 async fn main() {
+    // Route all requests on "/" endpoint to anonymous handler.
+    //
+    // A handler is an async function which returns something that implements
+    // `axum::response::IntoResponse`.
+
+    // A closure or a function can be used as handler.
     let app: Router = Router::new().route("/", get(handler));
 
+    // Address that server will bind to.
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("->> Listening on localhost - port:  {}", addr.port());
 
     // Use `hyper::server::Server` which is re-exported through `axum::Server` to serve the app.
     axum::Server::bind(&addr)
@@ -17,6 +32,24 @@ async fn main() {
     // axum:
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn handler() -> impl IntoResponse {
+    // Generate a random number between 1 and 100.
+    let mut rng = thread_rng();
+    let number = rng.gen_range(1..=100);
+
+    // Return a HTML response.
+    Html(format!(
+        r#"
+        <html>
+            <head>
+                <title>Guessing Game</title>
+            </head>
+            <body>
+                <h1>Guess a number between 1 and 100</h1>
+                <h2>{}</h2>
+            </body>
+        </html>
+    "#,
+        number
+    ))
 }
