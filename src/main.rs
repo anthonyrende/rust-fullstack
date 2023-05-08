4b31dd1355c0008a6f285252d94bf1f249aafca8
Anthony Rende <rende718@gmail.com>
2023-05-07 23:15:22 -0400

    Serve app with auxum::Server on '/' route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+// use std::net::SocketAddr;
+// use tower_http::cors::{AllowedOrigins, CorsLayer};
+
+#[tokio::main]
+async fn main() {
+    let app: Router = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // Use `hyper::server::Server` which is re-exported through `axum::Server` to serve the app.
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    // axum:
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
